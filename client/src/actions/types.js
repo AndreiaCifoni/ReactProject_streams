379e2d218dad780b77e84498c829fe5bde48f02e
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-01 17:30:09 -0400

    action creators done for CRUDE operations

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,10 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const CREATE_STREAM = "CREATE_STREAM";
+export const FETCH_STREAMS = "FETCH_STREAMS";
+export const FETCH_STREAM = "FETCH_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
+export const EDIT_STREAM = "EDIT_STREAM";
 
 // esse export é pra trocar as strings do reducer e action creator,
 // assim nao tem erro de digitação entre eles

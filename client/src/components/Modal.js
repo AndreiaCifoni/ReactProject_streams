385b74d26ee72b8f7460ada9772a80d6ea6c167c
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-04 13:22:54 -0400

    Modal reusable... React.Fragment

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push("/")}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       {/* o stopPropagation ai de baixo vai fazer parar o efeito bolha 
       do onClick de cima... se nao for adicionado ai embaixo, o de cima
       acaba passando pra children  */}
@@ -15,15 +11,10 @@ const Modal = (props) => {
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
 
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
 
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
+  const actions = (
+    //React.Fragment pode ser representado como os tags vazios "<></>"
+    //estamos usando essa tag ao invez de div pq o div tira o style usado no JSX
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onismiss={() => history.push("/")}
+      />
     </div>
   );
 };
